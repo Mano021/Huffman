731a10a383b64d7b645f33bb7398bbe3f034cda6
unknown <manojmano021@gmail.com>
2021-06-25 12:21:30 +0530

    Huffman Assignment - Manoj Kumar R

--- huffman.py
+++ huffman.py
@@ -1,42 +1,191 @@
-#!/usr/local/bin/python3
-import sys
-import argparse
-import shutil
+import os
+import heapq
+
+'''
+Manoj Kumar R
+Huffman :
+Replace the characters in the data with their code values
+Code Values are assigned in Bit Sequence
+Codes are assigned such a way that no code occurs as a prefix of any other code
+No ambiguity when we decode the data
+Reducing the overall size
+'''
+
+class Huffman:
+	def __init__(self, path):
+		# path of the file to compress
+		self.path = path
+		#intializing priority queue with empty
+		self.heap = []
+		#key will be the character and value will be code
+		self.codes = {}
+		self.reverse_mapping = {}
+		
+# creating a heap node with child and parent
+	class CreateHeap:
+		def __init__(self, char, freq):
+			self.char = char
+			self.freq = freq
+			self.left = None
+			self.right = None
+
+		# defining comparators less_than and equals
+		def __lt__(self, other):
+			return self.freq < other.freq
+
+		def __eq__(self, other):
+			if(other == None):
+				return False
+			if(not isinstance(other, CreateHeap)):
+				return False
+			return self.freq == other.freq
+
+	# functions for compression:
+
+	def set_frequency_dictionary(self, text):
+		frequency = {}
+		for character in text:
+			if not character in frequency:
+				frequency[character] = 0
+			frequency[character] += 1
+		return frequency
+
+# pushing the frequency digit output into heap
+	def make_heap(self, frequency):
+		for key in frequency:
+			node = self.CreateHeap(key, frequency[key])
+			heapq.heappush(self.heap, node)
+
+# keep on updating the priority queue until one node left
+	def merge_heap_nodes(self):
+		while(len(self.heap)>1):
+			node1 = heapq.heappop(self.heap)
+			node2 = heapq.heappop(self.heap)
+
+			merged = self.CreateHeap(None, node1.freq + node2.freq)
+			merged.left = node1
+			merged.right = node2
+
+			heapq.heappush(self.heap, merged)
 
 
-def encode(input_file, output_file):
-	print("encoding ", input_file, output_file)
-	# write code here
+	def make_codes_helper(self, root, current_code):
+		if(root == None):
+			return
 
-	# simply copying the file to bypass the actual test.
-	# remove the below lines.
-	if input_file != "" and output_file != "":
-		shutil.copyfile(input_file, output_file)
+		if(root.char != None):
+			self.codes[root.char] = current_code
+			self.reverse_mapping[current_code] = root.char
+			return
 
-
-def decode(input_file, output_file):
-	print("decoding ", input_file, output_file)
-	# write code here
-
-	# simply copying the file to bypass the actual test.
-	# remove the below lines.
-	if input_file != "" and output_file != "":
-		shutil.copyfile(input_file, output_file)
+		self.make_codes_helper(root.left, current_code + "0")
+		self.make_codes_helper(root.right, current_code + "1")
 
 
-def get_options(args=sys.argv[1:]):
-	parser = argparse.ArgumentParser(description="Huffman compression.")
-	groups = parser.add_mutually_exclusive_group(required=True)
-	groups.add_argument("-e", type=str, help="Encode files")
-	groups.add_argument("-d", type=str, help="Decode files")
-	parser.add_argument("-o", type=str, help="Write encoded/decoded file", required=True)
-	options = parser.parse_args()
-	return options
+	def make_codes(self):
+		root = heapq.heappop(self.heap)
+		current_code = ""
+		self.make_codes_helper(root, current_code)
 
 
-if __name__ == "__main__":
-	options = get_options()
-	if options.e is not None:
-		encode(options.e, options.o)
-	if options.d is not None:
-		decode(options.d, options.o)
+#replace character with code and return
+	def encode_text(self, text):
+		encoded_text = ""
+		for character in text:
+			encoded_text += self.codes[character]
+		return encoded_text
+
+# adding extra padding bit if length is not divisible by 8 
+	def padding_check(self, encoded_text):
+		extra_padding = 8 - len(encoded_text) % 8
+		for i in range(extra_padding):
+			encoded_text += "0"
+
+		padded_info = "{0:08b}".format(extra_padding)
+		encoded_text = padded_info + encoded_text
+		return encoded_text
+
+# take the encoded text and covert into byte and return byte array
+	def get_byte_array(self, padded_encoded_text):
+		if(len(padded_encoded_text) % 8 != 0):
+			print("Encoded text not padded properly")
+			exit(0)
+
+		b = bytearray()
+		for i in range(0, len(padded_encoded_text), 8):
+			byte = padded_encoded_text[i:i+8]
+			b.append(int(byte, 2))
+		return b
+
+# storing the output file in bin format in same location
+	def compress(self):
+		filename, file_extension = os.path.splitext(self.path)
+		output_path = filename + ".bin"
+
+		with open(self.path, 'r+') as file, open(output_path, 'wb') as output:
+			text = file.read()
+			text = text.rstrip()
+
+			frequency = self.set_frequency_dictionary(text)
+			self.make_heap(frequency)
+			self.merge_heap_nodes()
+			self.make_codes()
+
+			encoded_text = self.encode_text(text)
+			padded_encoded_text = self.padding_check(encoded_text)
+
+			b = self.get_byte_array(padded_encoded_text)
+			output.write(bytes(b))
+
+		print("Compressed and Output .bin file generated")
+		return output_path
+
+
+        #functions for decompression:
+
+
+	def remove_padding(self, padded_encoded_text):
+		padded_info = padded_encoded_text[:8]
+		extra_padding = int(padded_info, 2)
+
+		padded_encoded_text = padded_encoded_text[8:] 
+		encoded_text = padded_encoded_text[:-1*extra_padding]
+
+		return encoded_text
+
+	def decode_text(self, encoded_text):
+		current_code = ""
+		decoded_text = ""
+
+		for bit in encoded_text:
+			current_code += bit
+			if(current_code in self.reverse_mapping):
+				character = self.reverse_mapping[current_code]
+				decoded_text += character
+				current_code = ""
+
+		return decoded_text
+
+
+	def decompress(self, input_path):
+		filename, file_extension = os.path.splitext(self.path)
+		output_path = filename + "_decompressed" + ".txt"
+
+		with open(input_path, 'rb') as file, open(output_path, 'w') as output:
+			bit_string = ""
+
+			byte = file.read(1)
+			while(len(byte) > 0):
+				byte = ord(byte)
+				bits = bin(byte)[2:].rjust(8, '0')
+				bit_string += bits
+				byte = file.read(1)
+
+			encoded_text = self.remove_padding(bit_string)
+
+			decompressed_text = self.decode_text(encoded_text)
+			
+			output.write(decompressed_text)
+
+		print("Decompressed and Output .txt file generated")
+		return output_path
